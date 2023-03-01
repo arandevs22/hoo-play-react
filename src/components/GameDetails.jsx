f97abecf68455e67ff6dac51f57017492e1374b3
alfredo cruz <arandevs22@gmail.com>
2023-03-01 13:15:12 -0600

    details commit

--- src/components/GameDetails.jsx
+++ src/components/GameDetails.jsx
@@ -1,6 +1,7 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography, Container, IconButton } from "@mui/material";
 import { useLoaderData } from "react-router-dom";
 import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 
 export const GameDetails = () => {
@@ -9,22 +10,32 @@ export const GameDetails = () => {
 
     return (
         <>
-            <div className="uk-position-relative">
+            <div className="uk-position-relative uk-margin-bottom">
                 <div className="uk-background-cover uk-height-medium uk-panel uk-flex uk-flex-center uk-flex-middle " style={{ backgroundImage: `url(${game.background})` }}>
                 </div>
                 <div className="uk-overlay-primary uk-position-cover"></div>
                 <div className="uk-overlay uk-position-center">
                     <img className="gameLogo" src={game.logo} alt={game.title} />
                 </div>
-                <div className="uk-position-bottom-left">
-                    <Typography className="uk-margin-left" variant="h6" color={'#fff'}>
+                <div className="uk-position-top-left">
+                    <IconButton
+
+                    >
+                        <ArrowBackIosIcon />
+                    </IconButton>
+                </div>
+                <div className="uk-position-bottom-left uk-margin-left">
+                    <Typography className="subtitle" variant="body1" color={"#01EB78"}>
+                        {game.genre} · {game.year}
+                    </Typography>
+                    <Typography variant="h6" color={'#fff'}>
                         {game.title}
                     </Typography>
                 </div>
             </div>
-            <div>
+            <Container >
                 <Button fullWidth={true} href={game.reference} variant="contained" startIcon={<VideogameAssetIcon />}>Jugar Ahora</Button>
-            </div>
+            </Container>
         </>
     )
 }
