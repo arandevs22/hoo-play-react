6f9dcb3e339f00d4a14f27e25d7df30e32a66b06
alfredo cruz <arandevs22@gmail.com>
2023-03-01 18:22:58 -0600

    game details commit

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -13,7 +13,7 @@ export const TopNav = () => {
                     <Box sx={{ flexGrow: 1 }} >
                         <img className="logo" src="https://raw.githubusercontent.com/arandevs22/hoo_play/d4c9aa916aa9c1768a0d3309ecc6d95fe0352ac3/img/hoo_tv_logo.svg" alt="hoo-play-logo" />
                     </Box>
-                    <Link href="http://action_search" color="inherit">
+                    <Link href="http://action_search" color={"#01EB78"}>
                         <SearchIcon />
                     </Link>
                 </Toolbar>
