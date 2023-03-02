22349f0ad6b0cb9798b9d57151ba885374d03e7e
alfredo cruz <arandevs22@gmail.com>
2023-03-01 19:07:03 -0600

    game details commit

--- src/components/GameDetails.jsx
+++ src/components/GameDetails.jsx
@@ -8,6 +8,10 @@ export const GameDetails = () => {
 
     const { game } = useLoaderData();
 
+    const backButton = () => {
+        history.back();
+    }
+
     return (
         <>
             <div className="uk-position-relative uk-margin-bottom">
@@ -19,15 +23,24 @@ export const GameDetails = () => {
                 </div>
                 <div className="uk-position-top-left uk-margin-small-left uk-margin-small-top">
                     <IconButton
+                        onClick={backButton}
                     >
                         <ArrowBackIosIcon  />
                     </IconButton>
                 </div>
                 <div className="uk-position-bottom-left uk-margin-left">
-                    <Typography className="subtitle" variant="body2" color={"#01EB78"}>
-                        {game.genre} · {game.year} · {game.console}
+                    <Typography mb={2} >
+                        <Button className="detailsBtn uk-margin-small-right" variant="outlined">
+                            {game.genre}
+                        </Button>
+                        <Button className="detailsBtn uk-margin-small-right" variant="outlined">
+                            {game.year}
+                        </Button>
+                        <Button className="detailsBtn" variant="outlined">
+                            {game.console}
+                        </Button>
                     </Typography>
-                    <Typography variant="h5" color={'#fff'}>
+                    <Typography variant="h6" color={'#fff'}>
                         {game.title}
                     </Typography>
                 </div>
