b8082563962a8679eea4fe2f3498344db029c945
alfredo cruz <arandevs22@gmail.com>
2023-03-06 03:31:24 -0600

    movie commit

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,8 +1,16 @@
-import { Container, ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
-import { useLoaderData } from "react-router-dom";
-import { TopNav } from "../components/TopNav";
-import { Link } from "react-router-dom";
 import { Star } from "@mui/icons-material";
+import { Container, createTheme, Grid, Paper, styled, ThemeProvider, Typography } from "@mui/material";
+import { Link, useLoaderData } from "react-router-dom";
+import { TopNav } from "../components/TopNav";
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body3,
+    textAlign: 'left',
+    padding: '10px'
+}))
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } })
 
 export const Movies = () => {
 
@@ -12,18 +20,26 @@ export const Movies = () => {
         <>
             <TopNav />
             <Container>
-                <ImageList gap={20} cols={3}>
-                    {movies.items.map((movie) => (
-                        <Link to={`/peliculas/id/${movie.id}`} key={movie.id}>
-                            <ImageListItem>
-                                <img className="cover" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt={movie.title} />
-                                <ImageListItemBar
-                                    title={<div> <Star className="details" /> {movie.vote_average} </div>}
-                                />
-                            </ImageListItem>
-                        </Link>
-                    ))}
-                </ImageList>
+                <Grid mb={5} container spacing={1}>
+                    <ThemeProvider theme={darkTheme}>
+                        {movies.items.map((movie) => (
+                            <Grid item xs={4} key={movie.id}>
+                                <Item elevation={3}>
+                                    <Link to={`/peliculas/id/${movie.id}`}>
+                                        <img className="cover" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} />
+                                    </Link>
+                                    <Typography mt={1} className="title-movie">
+                                        {movie.title}
+                                    </Typography>
+                                    <Typography mt={0.5} className="overage">
+                                        <Star className="details" /> {movie.vote_average}
+                                    </Typography>
+                                </Item>
+                            </Grid>
+                        ))}
+
+                    </ThemeProvider>
+                </Grid>
             </Container>
         </>
     )
