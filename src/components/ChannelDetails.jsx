cf3091e81225cc2ee7909207f5e09644457a019e
alfredo cruz <arandevs22@gmail.com>
2023-03-06 19:22:32 -0600

    styles commit

--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Container, IconButton } from "@mui/material";
+import { Button, Typography, Container, IconButton, Box } from "@mui/material";
 import { Link, useLoaderData } from "react-router-dom";
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
@@ -14,7 +14,7 @@ export const ChannelDetails = () => {
 
     return (
         <>
-            <div className="uk-position-relative uk-margin-bottom">
+            <div className="uk-position-relative">
                 <div className="uk-background-cover uk-height-medium uk-panel uk-flex uk-flex-center uk-flex-middle " style={{ backgroundImage: `url(https://ik.imagekit.io/hooplay/o/hoo-play%2Fimagenes%2Fcanales%2Fbackground%2F${channel.img}.jpg?alt=media&token=8e656e30-8f35-4b8d-aee7-adace3393b9f)` }}>
                 </div>
                 <div className="uk-overlay-primary uk-position-cover"></div>
@@ -29,26 +29,24 @@ export const ChannelDetails = () => {
                     </IconButton>
                 </div>
                 <div className="uk-position-bottom-left uk-margin-left">
-                    <Typography mb={2} >
-                        <Link className="uk-link-reset" to={`/canales/genre/${channel.genre}`}>
-                            <Button className="detailsBtn uk-margin-small-right" variant="outlined">
-                                {channel.genre}
-                            </Button>
-                        </Link>
-                    </Typography>
-                    <Typography variant="h6" color={'#fff'}>
+                    <Typography mb={1} variant="h5" color={'#fff'}>
                         {channel.title}
                     </Typography>
                 </div>
             </div>
             <Container className="uk-margin-bottom" >
-                <Button fullWidth={true} href={channel.reference} variant="contained" startIcon={<PlayCircleOutlineIcon />}>
-                    reproducir canal
-                </Button>
-                <hr color={"#232323"} />
-                <Typography className="uk-margin-small-bottom" variant="body1">
-                    Información
+                <Typography mb={2} >
+                    <Link className="uk-link-reset" to={`/canales/genre/${channel.genre}`}>
+                        <Button className="detailsBtn uk-margin-small-right" variant="outlined">
+                            {channel.genre}
+                        </Button>
+                    </Link>
                 </Typography>
+                <Box mb={2}>
+                    <Button fullWidth={true} href={channel.reference} variant="contained" startIcon={<PlayCircleOutlineIcon />}>
+                        reproducir canal
+                    </Button>
+                </Box>
                 <Typography variant="body2" color={"gray"}>
                     {channel.sinopsis}
                 </Typography>
--- src/pages/channels-category/ChannelsGenre.jsx
+++ src/pages/channels-category/ChannelsGenre.jsx
@@ -1,6 +1,13 @@
-import { Container, ImageList, ImageListItem, Typography, } from "@mui/material";
+import { Container, createTheme, Paper, styled, Typography, Grid, ThemeProvider, } from "@mui/material";
 import { Link, useLoaderData } from "react-router-dom";
-import { TopNav } from "../../components/TopNav";
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    textAlign: 'left',
+    padding: '10px'
+}))
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
 export const ChannelsGenre = () => {
 
@@ -8,20 +15,26 @@ export const ChannelsGenre = () => {
 
     return (
         <>
-            <TopNav />
-            <Container>
-                <Typography className="genreTitle" variant="h6">
+            <Container className="uk-margin-top">
+                <Typography mb={2} className="genreTitle" variant="h6">
                     Canales {channelsGenre[0].genre}
                 </Typography>
-                <ImageList gap={10} cols={3}>
-                    {channelsGenre.map((channel) => (
-                        <Link to={`/canales/id/${channel.id}`} key={channel.img}>
-                            <ImageListItem>
-                                <img className="cover" src={`https://ik.imagekit.io/hooplay/o/hoo-play%2Fimagenes%2Fcanales%2Flogos%2F${channel.img}.jpg?alt=media&token=c1c23d46-cac3-4d48-8390-10a5f9ddfa65`} alt={channel.title} />
-                            </ImageListItem>
-                        </Link>
-                    ))}
-                </ImageList>
+                <Grid mb={5} container spacing={1} >
+                    <ThemeProvider theme={darkTheme}>
+                        {channelsGenre.map((channel) => (
+                            <Grid item xs={4} key={channel.id}>
+                                <Item elevation={3}>
+                                    <Link to={`/canales/id/${channel.id}`}>
+                                        <img className="cover" src={`https://ik.imagekit.io/hooplay/o/hoo-play%2Fimagenes%2Fcanales%2Flogos%2F${channel.img}.jpg?alt=media&token=c1c23d46-cac3-4d48-8390-10a5f9ddfa65`} />
+                                    </Link>
+                                    <Typography mt={1} className="title-movie">
+                                        {channel.title}
+                                    </Typography>
+                                </Item>
+                            </Grid>
+                        ))}
+                    </ThemeProvider>
+                </Grid>
             </Container>
         </>
     )
