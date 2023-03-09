4e37ea88d7bc7525da20edbb88db08741fd13482
alfredo cruz <arandevs22@gmail.com>
2023-03-09 13:47:42 -0600

    series commit

--- src/components/SeriesSeasons.jsx
+++ src/components/SeriesSeasons.jsx
@@ -36,43 +36,35 @@ export const SeriesSeasons = () => {
                 <Typography mb={2} variant="body2" color={"rgba(255, 255, 255, 0.7)"}>
                     {seriesSeasons.overview}
                 </Typography>
-                <div className="uk-margin-bottom" data-uk-slider>
-                    <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex='-1'>
-                        <ul className="uk-slider-items uk-child-width-1-1 uk-grid-small" data-uk-grid>
-                            {seriesSeasons.episodes.map((episode) => (
-                                <li>
-                                    <Box>
-                                        <Card >
-                                            <CardMedia
-                                                sx={{ height: 140 }}
-                                                image={`https://image.tmdb.org/t/p/original${episode.still_path}`}
-                                                title={episode.name}
-                                            />
-                                            <CardContent>
-                                                <Typography gutterBottom variant="h6" component="div">
-                                                    {episode.name}
-                                                </Typography>
-                                                <Typography variant="subtitle1" color={"rgba(255, 255, 255, 0.7)"}>
-                                                    Season {seriesSeasons.season_number} · Episode {episode.episode_number}
-                                                </Typography>
-                                                <Typography variant="subtitle2" color={"rgba(255, 255, 255, 0.7)"}>
-                                                    {episode.air_date}
-                                                </Typography>
-                                            </CardContent>
-                                            <CardActions>
-                                                <Button size="small" className="season">
-                                                    ver episodio
-                                                </Button>
-                                            </CardActions>
-                                        </Card>
-                                    </Box>
-                                </li>
-                            ))}
-                        </ul>
-                        <a className="uk-position-top-left uk-position-small" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
-                        <a className="uk-position-top-right uk-position-small" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
-                    </div>
-                </div>
+                <Box mb={3}>
+                    {seriesSeasons.episodes.map((episode) => (
+                        <Box mb={1}>
+                            <Card >
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image={`https://image.tmdb.org/t/p/w400${episode.still_path}`}
+                                    title={episode.name}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h6" component="div">
+                                        {episode.name}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color={"rgba(255, 255, 255, 0.7)"}>
+                                        Season {seriesSeasons.season_number} · Episode {episode.episode_number}
+                                    </Typography>
+                                    <Typography variant="subtitle2" color={"rgba(255, 255, 255, 0.7)"}>
+                                        {episode.air_date}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" className="season">
+                                        ver episodio
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Box>
+                    ))}
+                </Box>
             </Container>
         </>
     )
