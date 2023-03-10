443bd916ddfc2f0b9b8a9a66df9c9094cf54040f
alfredo cruz <arandevs22@gmail.com>
2023-03-09 22:58:31 -0600

    games commit

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -48,6 +48,11 @@ export const BottomNav = () => {
                         icon={<OndemandVideoIcon />}
                         onClick={() => navigate('/series')}
                     />
+                    <BottomNavigationAction
+                        label="Juegos"
+                        icon={<SportsEsportsIcon />}
+                        onClick={() => navigate('/juegos')}
+                    />
                 </BottomNavigation>
             </Paper>
             <Offset />
--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,5 +1,16 @@
-import { Container, ImageList, ImageListItem, } from "@mui/material";
+import { Star } from "@mui/icons-material";
+import { Container, createTheme, Grid, Paper, styled, ThemeProvider, Typography, AppBar, Toolbar, } from "@mui/material";
 import { Link, useLoaderData } from "react-router-dom";
+import { TopNav } from "../components/TopNav";
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body3,
+    textAlign: 'left',
+    padding: '10px'
+}))
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } })
 
 export const Games = () => {
 
@@ -7,23 +18,32 @@ export const Games = () => {
 
     return (
         <>
-            <Container>
-                <ImageList gap={10} cols={3}>
-                    {games.map((game) => (
-                        <Link to={`/juegos/id/${game.id}`} key={game.id}>
-                            <ImageListItem>
-                                <img className="cover" src={`https://ik.imagekit.io/hooplay/o/hoo-play%2Fimagenes%2Fjuegos%2Fposter%2F${game.id}.jpg?alt=media&token=c1c23d46-cac3-4d48-8390-10a5f9ddfa65`} alt={game.title} />
-                            </ImageListItem>
-                        </Link>
-                    ))}
-                </ImageList>
+            <TopNav />
+            <Container >
+                <Grid mb={5} container spacing={1}>
+                    <ThemeProvider theme={darkTheme}>
+                        {games.map((game) => (
+                            <Grid item xs={4} key={game.id}>
+                                <Item elevation={3}>
+                                    <Link to={`/juegos/id/${game.id}`}>
+                                        <img className="cover" src={`https://ik.imagekit.io/hooplay/o/hoo-play%2Fimagenes%2Fjuegos%2Fposter%2F${game.id}.jpg?alt=media&token=c1c23d46-cac3-4d48-8390-10a5f9ddfa65`} />
+                                    </Link>
+                                    <Typography mt={1} className="title-movie">
+                                        {game.title}
+                                    </Typography>
+                                </Item>
+                            </Grid>
+                        ))}
+
+                    </ThemeProvider>
+                </Grid>
             </Container>
         </>
     )
 }
 
 export const loaderGames = async () => {
-    const res = await fetch(`https://apiretrogame-production.up.railway.app/api/games/game-boy-advance/`);
+    const res = await fetch(`https://apiretrogame-production.up.railway.app/api/games/game-boy-advance`);
 
     const games = await res.json();
 
